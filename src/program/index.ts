a5b8be3ece264a56e021a33f1944fb1eef397897
Zhonghao Cao <coderczh@gmail.com>
2022-11-13 17:15:45 +0800

    add texture and lighting

--- src/program/index.ts
+++ src/program/index.ts
@@ -1,17 +1,20 @@
 import { mat4 } from "gl-matrix";
 
-import { loadShader, createWebGLProgram } from "../shaders";
+import { loadShader, createWebGLProgram, loadTexture } from "../shaders";
 
 class Program {
   gl: WebGL2RenderingContext;
   program: WebGLProgram;
   attribLocations: {
     vertexPosition: number;
-    vertexColor: number;
+    vertexNormal: number;
+    textureCoord: number;
   };
   uniformLocations: {
     projectionMatrix: WebGLUniformLocation;
     modelViewMatrix: WebGLUniformLocation;
+    normalMatrix: WebGLUniformLocation;
+    uSampler: WebGLUniformLocation;
   };
   cubeRotation = 0;
   constructor(gl: WebGL2RenderingContext, vsSource: string, fsSource: string) {
@@ -22,7 +25,8 @@ class Program {
 
     this.attribLocations = {
       vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
-      vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+      vertexNormal: gl.getAttribLocation(shaderProgram, "aVertexNormal"),
+      textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
     };
 
     this.uniformLocations = {
@@ -34,12 +38,21 @@ class Program {
         shaderProgram,
         "uModelViewMatrix"
       ) as WebGLUniformLocation,
+      normalMatrix: gl.getUniformLocation(
+        shaderProgram,
+        "uNormalMatrix"
+      ) as WebGLUniformLocation,
+      uSampler: gl.getUniformLocation(
+        shaderProgram,
+        "uSampler"
+      ) as WebGLUniformLocation,
     };
   }
 
   bindPositions(
     positions: Array<number>,
-    colors: Array<number>,
+    vertexNormals: Array<number>,
+    textureCoordinates: Array<number>,
     indices: Array<number>
   ) {
     const positionBuffer = this.gl.createBuffer();
@@ -50,13 +63,21 @@ class Program {
       this.gl.STATIC_DRAW
     );
 
-    const colorBuffer = this.gl.createBuffer();
-    this.gl.bindBuffer(WebGLRenderingContext.ARRAY_BUFFER, colorBuffer);
+    const normalBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, normalBuffer);
     this.gl.bufferData(
-      WebGLRenderingContext.ARRAY_BUFFER,
-      new Float32Array(colors),
-      WebGLRenderingContext.STATIC_DRAW
+      this.gl.ARRAY_BUFFER,
+      new Float32Array(vertexNormals),
+      this.gl.STATIC_DRAW
     );
+    const textureCoordBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, textureCoordBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER,
+      new Float32Array(textureCoordinates),
+      this.gl.STATIC_DRAW
+    );
+
     const indexBuffer = this.gl.createBuffer();
     this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
     this.gl.bufferData(
@@ -67,7 +88,8 @@ class Program {
 
     return {
       position: positionBuffer,
-      color: colorBuffer,
+      normal: normalBuffer,
+      textureCoord: textureCoordBuffer,
       indices: indexBuffer,
     };
   }
@@ -75,9 +97,11 @@ class Program {
   drawScene(
     buffers: {
       position: WebGLBuffer;
-      color: WebGLBuffer;
+      normal: WebGLBuffer;
+      textureCoord: WebGLBuffer;
       indices: WebGLBuffer;
     },
+    texture: WebGLTexture,
     deltaTime = 0
   ) {
     this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
@@ -106,7 +130,10 @@ class Program {
       this.cubeRotation * 0.3,
       [1, 0, 0]
     );
-    this.cubeRotation += deltaTime;
+    const normalMatrix = mat4.create();
+    mat4.invert(normalMatrix, modelViewMatrix);
+    mat4.transpose(normalMatrix, normalMatrix);
+
     {
       const numComponents = 3;
       const type = this.gl.FLOAT;
@@ -125,22 +152,41 @@ class Program {
       this.gl.enableVertexAttribArray(this.attribLocations.vertexPosition);
     }
     {
-      const numComponents = 4;
+      const numComponents = 2;
       const type = this.gl.FLOAT;
       const normalize = false;
       const stride = 0;
       const offset = 0;
-      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffers.color);
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffers.textureCoord);
       this.gl.vertexAttribPointer(
-        this.attribLocations.vertexColor,
+        this.attribLocations.textureCoord,
         numComponents,
         type,
         normalize,
         stride,
         offset
       );
-      this.gl.enableVertexAttribArray(this.attribLocations.vertexColor);
+      this.gl.enableVertexAttribArray(this.attribLocations.textureCoord);
     }
+
+    {
+      const numComponents = 3;
+      const type = this.gl.FLOAT;
+      const normalize = false;
+      const stride = 0;
+      const offset = 0;
+      this.gl.bindBuffer(this.gl.ARRAY_BUFFER, buffers.normal);
+      this.gl.vertexAttribPointer(
+        this.attribLocations.vertexNormal,
+        numComponents,
+        type,
+        normalize,
+        stride,
+        offset
+      );
+      this.gl.enableVertexAttribArray(this.attribLocations.vertexNormal);
+    }
+
     this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, buffers.indices);
 
     this.gl.useProgram(this.program);
@@ -155,12 +201,24 @@ class Program {
       false,
       modelViewMatrix
     );
+    this.gl.uniformMatrix4fv(
+      this.uniformLocations.normalMatrix,
+      false,
+      normalMatrix
+    );
+
+    this.gl.activeTexture(this.gl.TEXTURE0);
+    this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+    this.gl.uniform1i(this.uniformLocations.uSampler, 0);
+
     {
       const vertexCount = 36;
       const type = this.gl.UNSIGNED_SHORT;
       const offset = 0;
       this.gl.drawElements(this.gl.TRIANGLES, vertexCount, type, offset);
     }
+
+    this.cubeRotation += deltaTime;
   }
 
   draw() {
@@ -178,13 +236,39 @@ class Program {
       // Left face
       -1.0, -1.0, -1.0, -1.0, -1.0, 1.0, -1.0, 1.0, 1.0, -1.0, 1.0, -1.0,
     ];
-    const faceColors = [
-      [1.0, 1.0, 1.0, 1.0], // Front face: white
-      [1.0, 0.0, 0.0, 1.0], // Back face: red
-      [0.0, 1.0, 0.0, 1.0], // Top face: green
-      [0.0, 0.0, 1.0, 1.0], // Bottom face: blue
-      [1.0, 1.0, 0.0, 1.0], // Right face: yellow
-      [1.0, 0.0, 1.0, 1.0], // Left face: purple
+    const vertexNormals = [
+      // Front
+      0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+
+      // Back
+      0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0,
+
+      // Top
+      0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
+
+      // Bottom
+      0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0,
+
+      // Right
+      1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+
+      // Left
+      -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0,
+    ];
+
+    const textureCoordinates = [
+      // Front
+      0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+      // Back
+      0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+      // Top
+      0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+      // Bottom
+      0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+      // Right
+      0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
+      // Left
+      0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 0.0, 1.0,
     ];
     // prettier-ignore
     const indices = [
@@ -195,20 +279,23 @@ class Program {
       16, 17, 18, 16, 18, 19, 
       20, 21, 22, 20, 22, 23,
     ];
-    let colors: number[] = [];
-    for (let j = 0; j < faceColors.length; ++j) {
-      const c = faceColors[j];
-      colors = colors.concat(c, c, c, c);
-    }
-    const buffers = this.bindPositions(positions, colors, indices);
+    const buffers = this.bindPositions(
+      positions,
+      vertexNormals,
+      textureCoordinates,
+      indices
+    );
+    const texture = loadTexture(
+      this.gl,
+      "http://localhost:8080/cubetexture.png"
+    );
     let then = 0;
-    console.log(buffers);
     const render = (now: number) => {
       now *= 0.001;
       const deltaTime = now - then;
       then = now;
 
-      this.drawScene(buffers, deltaTime);
+      this.drawScene(buffers, texture, deltaTime);
 
       requestAnimationFrame(render);
     };
